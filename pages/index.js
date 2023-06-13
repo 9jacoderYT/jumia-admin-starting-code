d56ca62dbdb39a84dd57a30ce151ed5bddb08e13
Smart Oghenemine <35765212+VinciDemons@users.noreply.github.com>
2023-06-13 21:39:53 +0100

    First commit

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,4 @@
-import AllProducts from "../components/AllProducts";
-import { fetchOrders, fetchProducts } from "../lib/fetchData";
-
-export default function Home({ products, orders }) {
+export default function Home() {
   return (
     <div className="min-h-screen">
       <p className="text-gray-700 text-3xl mb-16 font-bold">Dashboard</p>
@@ -10,7 +7,7 @@ export default function Home({ products, orders }) {
         <div className="stats shadow">
           <div className="stat">
             <div className="stat-title">Total Products </div>
-            <div className="stat-value">{products.length}</div>
+            <div className="stat-value">0</div>
             <div className="stat-desc">21% more than last month</div>
           </div>
         </div>
@@ -18,25 +15,11 @@ export default function Home({ products, orders }) {
         <div className="stats shadow">
           <div className="stat">
             <div className="stat-title">Total Active Orders </div>
-            <div className="stat-value">{orders.length}</div>
+            <div className="stat-value">0</div>
             <div className="stat-desc">21% more than last month</div>
           </div>
         </div>
       </div>
-
-      <AllProducts products={products} />
     </div>
   );
-}
-
-export async function getServerSideProps() {
-  const products = await fetchProducts();
-  const orders = await fetchOrders();
-
-  return {
-    props: {
-      products,
-      orders,
-    },
-  };
 }
--- pages/orders/index.js
+++ pages/orders/index.js
@@ -1,21 +1,8 @@
-import React from "react";
-import { fetchOrders } from "../../lib/fetchData";
-import AllOrders from "../../components/AllOrders";
 
 export default function Orders({ orders }) {
   return (
     <div className="min-h-screen">
-      <AllOrders orders={orders} />
+      orders
     </div>
   );
-}
-
-export async function getServerSideProps() {
-  const orders = await fetchOrders();
-
-  return {
-    props: {
-      orders,
-    },
-  };
 }
